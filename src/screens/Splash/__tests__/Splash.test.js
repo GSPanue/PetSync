7a0cf5da526b9dc18c4aae10dfad2a8a5ef58d2d
Gurdev S. Panue <gpanue@hotmail.com>
2018-08-13 02:21:51 +0100

    Added Splash component tests

--- src/screens/Splash/__tests__/Splash.test.js
+++ src/screens/Splash/__tests__/Splash.test.js
@@ -1,17 +1,39 @@
 import React from 'react';
-import {configure, shallow} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import {shallow} from 'enzyme';
 
 import {Splash} from '../Splash';
 
-configure({
-    adapter: new Adapter()
-});
-
 describe('Component: Splash', () => {
-    const wrapper = shallow(<Splash/>);
+    const minProps = {
+        animationType: null,
+        animationComplete: false,
+        changeAnimation: () => {}
+    };
 
     it('should render a single Splash component', () => {
+        const wrapper = shallow(<Splash {...minProps}/>);
+
         expect(wrapper).toHaveLength(1);
+    });
+
+    it('should contain a Fade component', () => {
+        const wrapper = shallow(<Splash {...minProps}/>);
+
+        expect(wrapper.find('Connect(Fade)')).toHaveLength(1);
+    });
+
+    it('should contain a Logo component', () => {
+        const wrapper = shallow(<Splash {...minProps}/>);
+
+        expect(wrapper.find('Logo')).toHaveLength(1);
+    });
+
+    it('should have props for animationType, animationComplete, and changeAnimation', () => {
+        const wrapper = shallow(<Splash {...minProps}/>);
+        const instance = wrapper.instance();
+
+        expect(instance.props.animationType);
+        expect(instance.props.animationComplete);
+        expect(instance.props.changeAnimation);
     });
 });
