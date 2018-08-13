a085f935aba4d97fd5a85a81500b7411b31d6eb8
Gurdev S. Panue <gpanue@hotmail.com>
2018-08-13 02:19:06 +0100

    Wrapped Logo inside Fade component and connected Splash to Redux store

--- src/screens/Splash/Splash.js
+++ src/screens/Splash/Splash.js
@@ -1,18 +1,62 @@
 import React from 'react';
-import {View} from 'react-native';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+
+import {changeAnimation} from '../../actions';
+import {allowNullPropType} from '../../helpers';
 
 import styles from './styles';
-
-import Logo from '../../components/Logo/Logo';
+import Fade from '../../components/Fade';
+import Logo from '../../components/Logo';
 
 export class Splash extends React.Component {
+    static propTypes = {
+        animationType: allowNullPropType(PropTypes.string.isRequired),
+        animationComplete: PropTypes.bool.isRequired,
+        changeAnimation: PropTypes.func.isRequired
+    };
+
+    componentDidMount() {
+        const {changeAnimation} = this.props;
+
+        /**
+         * ToDo: Fade out component and change screen after checking if the user is logged in.
+         */
+    }
+
+    componentDidUpdate() {
+        const {animationType, animationComplete} = this.props;
+        const shouldChangeScreen = (animationType === 'out' && animationComplete);
+
+        if (shouldChangeScreen) {
+            const {navigation} = this.props;
+
+            /**
+             * ToDo: Navigate to appropriate screen, e.g. navigation.navigate('SignIn');
+             */
+        }
+    }
+
     render() {
         return (
-            <View style={styles.container}>
-                <Logo inactive={false} enableFadeAnimation={true}/>
-            </View>
+            <Fade id='splash' styles={styles.container}>
+                <Logo height='125' width='125'/>
+            </Fade>
         );
     }
 }
 
-export default Splash;
+const mapStateToProps = (state) => {
+    return {
+        animationType: state.fade.getIn(['splash', 'type']),
+        animationComplete: state.fade.getIn(['splash', 'complete'])
+    }
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        changeAnimation: (id, type) => dispatch(changeAnimation(id, type))
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Splash);
