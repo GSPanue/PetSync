d99640a494c9e740fffd81aec9c3defe0e21a027
Gurdev S. Panue <gpanue@hotmail.com>
2018-08-22 10:33:10 +0100

    Implemented Zoom higher-order component without Redux

--- src/components/withZoom/withZoom.js
+++ src/components/withZoom/withZoom.js
@@ -1,18 +1,60 @@
-import {connect} from 'react-redux';
+import React from 'react';
+import PropTypes from 'prop-types';
+import * as Animatable from 'react-native-animatable';
 
-import Zoom from '../Zoom';
+import {hasCallback} from '../../helpers';
+import {zoomInScreen} from '../../config/animations';
+
+Animatable.initializeRegistryWithDefinitions({
+    zoomIn: zoomInScreen
+});
 
 const withZoom = (props) => (WrappedComponent) => {
-    const mapStateToProps = (state) => {
-        return {
-            wrappedComponent: WrappedComponent,
-            currentScreen: state.screen.get('currentScreen'),
-            nextScreen: state.screen.get('nextScreen'),
-            ...props
-        }
-    };
+    return class Zoom extends React.Component {
+        static propTypes = {
+            navigation: PropTypes.object
+        };
 
-    return connect(mapStateToProps)(Zoom);
+        constructor() {
+            super();
+
+            this.view = React.createRef();
+            this.zoomOutScreen = this.zoomOutScreen.bind(this);
+        }
+
+        shouldComponentUpdate() {
+            return false;
+        }
+
+        zoomOutScreen(callback) {
+            const {zoomOut} = this.view.current;
+
+            // Zooms out WrappedComponent
+            zoomOut(300).done(() => {
+                (hasCallback(callback)) && callback();
+            });
+        }
+
+        render() {
+            const {navigation} = this.props;
+            const {style} = props;
+
+            return (
+                <Animatable.View
+                    ref={this.view}
+                    animation='zoomIn'
+                    duration={200}
+                    useNativeDriver={true}
+                    style={[style]}
+                >
+                    <WrappedComponent
+                        navigation={navigation}
+                        zoomOutScreen={this.zoomOutScreen}
+                    />
+                </Animatable.View>
+            );
+        }
+    }
 };
 
 export default withZoom;
