a97b5ac79ab310a19594de57940dbcaa9a038101
Gurdev S. Panue <gpanue@hotmail.com>
2018-08-25 06:15:16 +0100

    Added SignIn stack

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {createStackNavigator, createSwitchNavigator} from 'react-navigation';
 
 import Splash from '../screens/Splash';
+import SignIn from '../screens/SignIn';
 
 export const SplashStack = createStackNavigator({
     splash: {
@@ -14,10 +15,24 @@ export const SplashStack = createStackNavigator({
     }
 });
 
+export const SignInStack = createStackNavigator({
+    signIn: {
+        screen: SignIn
+    }
+}, {
+    headerMode: 'none',
+    cardStyle: {
+        backgroundColor: 'transparent'
+    }
+});
+
 export const createRootNavigator = () => {
     return createSwitchNavigator({
         Splash: {
             screen: SplashStack
+        },
+        SignIn: {
+            screen: SignInStack
         }
     }, {
         initialRouteName: 'Splash'
