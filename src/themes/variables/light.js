17a930fee676b38bc9d061be27d538a99ceed600
Gurdev S. Panue <gpanue@hotmail.com>
2018-08-25 06:01:07 +0100

    Changed NativeBase input styles

--- src/themes/variables/light.js
+++ src/themes/variables/light.js
@@ -174,10 +174,10 @@ export default {
 
   // InputGroup
   inputFontSize: 17,
-  inputBorderColor: "#D9D5DC",
+  inputBorderColor: "#CCCCCC",
   inputSuccessBorderColor: "#2b8339",
   inputErrorBorderColor: "#ed2f2f",
-  inputHeightBase: 50,
+  inputHeightBase: 46,
   get inputColor() {
     return this.textColor;
   },
