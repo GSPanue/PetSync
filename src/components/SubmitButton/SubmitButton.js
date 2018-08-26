a9c27de5da51d2ee029ea694c7b2f839ae862fab
Gurdev S. Panue <gpanue@hotmail.com>
2018-08-26 19:56:08 +0100

    Added SubmitButton component

---
+++ src/components/SubmitButton/SubmitButton.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import PropTypes from 'prop-types';
+import {Button} from 'native-base';
+
+import styles from './styles';
+
+class SubmitButton extends React.Component {
+    static propTypes = {
+        title: PropTypes.string.isRequired,
+        position: PropTypes.string,
+        width: PropTypes.string,
+        submit: PropTypes.func.isRequired
+    };
+
+    static defaultProps = {
+        position: 'right',
+        width: '35%'
+    };
+
+    constructor() {
+        super();
+
+        this.handlePress = this.handlePress.bind(this);
+        this.getAlignment = this.getAlignment.bind(this);
+    }
+
+    handlePress() {
+        const {submit} = this.props;
+
+        submit();
+    }
+
+    getAlignment() {
+        const {position} = this.props;
+
+        if (position === 'center' || position === 'right') {
+            return (position === 'center') ? 'center' : 'flex-end';
+        }
+
+        return 'flex-start';
+    }
+
+    render() {
+        const {title, width} = this.props;
+        const {getAlignment} = this;
+
+        const alignment = getAlignment();
+
+        return (
+            <View style={[styles.container, {justifyContent: alignment}]}>
+                <View style={{width: width}}>
+                    <Button full rounded onPress={this.handlePress}>
+                        <Text style={styles.title}>{title}</Text>
+                    </Button>
+                </View>
+            </View>
+        );
+    }
+}
+
+export default SubmitButton;
---
+++ src/components/SubmitButton/styles.js
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        width: '100%'
+    },
+    title: {
+        fontSize: 17,
+        color: '#FFFFFF'
+    }
+});
