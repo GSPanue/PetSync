a72756491364695b75b3ad81af7c002832ea597e
Gurdev S. Panue <gpanue@hotmail.com>
2018-09-01 13:31:15 +0100

    Added react-native-animatable configuration

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import {StyleProvider} from 'native-base';
 import {createRootNavigator} from './config/routes';
+import {initializeRegistryWithDefinitions} from 'react-native-animatable';
 
 import store from './config/store';
 
 import getTheme from './themes/components/index';
 import lightTheme from './themes/variables/light';
+
+import {zoomIn} from './config/animations';
+
+initializeRegistryWithDefinitions({
+    zoomIn: zoomIn
+});
 
 export class App extends React.Component {
     render() {
