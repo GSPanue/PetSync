79a39d4cda5615445a9ac2b4727bf59e1a63e8ff
Gurdev S. Panue <gpanue@hotmail.com>
2018-09-04 14:55:11 +0100

    Removed validation helpers

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,2 +1,1 @@
 export * from './propTypes';
-export * from './validation';
--- src/helpers/validation/validation.js
+++
@@ -1,7 +0,0 @@
-export const hasCallback = (callback) => {
-    return (typeof callback === 'function');
-};
-
-export const isString = (value) => {
-    return (typeof value === 'string');
-};
