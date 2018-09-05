89a9b7f13187b439aba320bdfe289c443d4166b0
Gurdev S. Panue <gpanue@hotmail.com>
2018-09-05 18:24:46 +0100

    Changed NativeBase spinner color

--- src/themes/variables/light.js
+++ src/themes/variables/light.js
@@ -223,7 +223,7 @@ export default {
   segmentBorderColorMain: platform === "ios" ? "#a7a6ab" : "#3F51B5",
 
   // Spinner
-  defaultSpinnerColor: "#45D56E",
+  defaultSpinnerColor: "#D1A5CC",
   inverseSpinnerColor: "#1A191B",
 
   // Tab
